8e092b3d76b785ac1f5c07fcdcbbc6be11090f17
sambhuka <sambhu@netsend.com>
2016-08-29 18:18:43 +0100

    add missing function

--- app/Serialization/Serialize.php
+++ app/Serialization/Serialize.php
@@ -4,5 +4,20 @@ namespace DataStructure\Serialization;
 
 class Serialize
 {
+    protected $inputData;
 
+    public function __construct($inputData)
+    {
+        $this->inputData = $inputData;
+    }
+
+    public function xmlOutput()
+    {
+        return "";
+    }
+
+    public function intoXML()
+    {
+        return $this->xmlOutput();
+    }
 }
