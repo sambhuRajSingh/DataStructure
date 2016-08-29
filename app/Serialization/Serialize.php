b7f827756f06bf4f1f06b448d48c39bb35d2b783
sambhuka <sambhu@netsend.com>
2016-08-29 18:34:58 +0100

    starting and ending tag

--- app/Serialization/Serialize.php
+++ app/Serialization/Serialize.php
@@ -19,16 +19,55 @@ class Serialize
 
         } elseif (is_array($inputData)) {
             $output .= $this->startTag($inputData['name'], $depth);
+
+            $output .= $this->endTag($inputData['name'], $depth);
         }
 
         return $output;
     }
 
+    /**
+     * Formatted or unformatted opening xml tag
+     *
+     * @param string    $tagName
+     * @param int       $depth
+     * @return string
+     */
     private function startTag($tagName, $depth)
     {
-        return $this->tabs($depth - 1).'<'.htmlspecialchars($tagName).'>'."\n";
+        $startTag = '<'.htmlspecialchars($tagName).'>';
+
+        if ($this->formatted) {
+            return $this->tabs($depth - 1). $startTag ."\n";
+        } else {
+            return $startTag;
+        }
     }
 
+    /**
+     * Formatted or unformatted closing xml tag
+     *
+     * @param string    $tagName
+     * @param int       $depth
+     * @return string
+     */
+    private function endTag($tagName, $depth)
+    {
+        $endTag = '</'.htmlspecialchars($tagName).'>';
+
+        if ($this->formatted) {
+            return $this->tabs($depth - 1). $endTag ."\n";
+        } else {
+            return $endTag;
+        }
+    }
+
+    /**
+     * Return number of tabs.
+     *
+     * @param int   $depth
+     * @return string
+     */
     public function tabs($depth)
     {
         $tabs = "";
